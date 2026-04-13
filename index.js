ab6218ef19dca0824b173f6a227c26ac6bb3b7bb
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-04-13 11:31:54 +0300

    Added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...args[i]
+    ];
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
